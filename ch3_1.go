d6687824ac9a6034972253a983eea8e101b265c6
Jiekun <jiekun.dev@gmail.com>
2021-12-14 08:49:19 +0800

    Added ch3_1 codes

---
+++ ch3_1.go
@@ -0,0 +1,36 @@
+// @Author: Jiekun
+// @Date: 2021/12/14
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		kubeconfig := flag.String("kubeconfig", "/Users/jiekunzhu/.kube/config", "kubeconfig file")
+		flag.Parse()
+		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			fmt.Printf("err: %v", err)
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("err: %v", err)
+	}
+
+	pod, err := clientset.CoreV1().Pods("default").Get(context.Background(), "kubia-5bb46d6998-mszk7", metav1.GetOptions{})
+	fmt.Println(pod)
+}
